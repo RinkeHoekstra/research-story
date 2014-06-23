4131cdd13b9405953d8c32c38221eecb61bf1d7c
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-06-23 15:13:46 +0200

    Hacky but slightly smarter handling of RDF loading

--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, make_response, jsonify
 from rdflib import Graph
+import requests
+
 from app import app, socketio
 import sockets
 
@@ -11,9 +13,14 @@ def index():
 def retrieve():
     url = request.args.get('url',None)
     
+    r = requests.get(url)
+    
+    content_type = r.headers['content-type']
+    (content_type,_) = content_type.split(';',1)
+    
     if url:
         graph = Graph()
-        graph.load(url,format='rdfa')
+        graph.load(url,format=content_type)
         turtle = graph.serialize(format='turtle')
         
         print turtle
