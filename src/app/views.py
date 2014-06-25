ab59023afb2e698759351bc89581d9f76c9c2a9c
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-06-25 12:07:23 +0200

    Better headers, more elaborate query and better response (fixed image issue)

--- src/app/views.py
+++ src/app/views.py
@@ -1,12 +1,14 @@
 from flask import render_template, request, make_response, jsonify
 from rdflib import Graph, Namespace
 import requests
+import re
 
 from app import app, socketio
 import sockets
 
 PROV = Namespace('http://www.w3.org/ns/prov#')
 OG = Namespace('http://ogp.me/ns#')
+BIBO = Namespace('http://purl.org/ontology/bibo/')
 
 @app.route('/')
 def index():
@@ -22,18 +24,15 @@ def retrieve():
     description = request.args.get('description',None)
     image = request.args.get('image',None)
     license = None
+    date = None
+    creator = None
+    parent = None
+    publisher = None
     
-    headers = {'Accept':'application/rdf+xml, application/xhtml+xml;q=0.3, text/xml;q=0.2,application/xml;q=0.2, text/html;q=0.3, text/plain;q=0.1, text/n3, text/rdf+n3;q=0.5, application/x-turtle;q=0.2, text/turtle;q=1'}
+    headers = {'Accept':'application/rdf+xml;q=0.5, application/xhtml+xml;q=0.3, text/xml;q=0.2,application/xml;q=0.2, text/html;q=0.3, text/plain;q=0.1, text/n3;q=0.5, text/rdf+n3;q=0.5, application/x-turtle;q=0.8, text/turtle;q=1'}
     
     
-    r = requests.get(url, headers=headers)
     
-    content_type = r.headers['content-type']
-    
-    print content_type
-    
-    if ';' in content_type:
-        (content_type,_) = content_type.split(';',1)
     
 
     query = """
@@ -44,20 +43,38 @@ def retrieve():
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         PREFIX foaf: <http://xmlns.com/foaf/0.1/>
         PREFIX cc: <http://creativecommons.org/ns#>
+        PREFIX bibo: <http://purl.org/ontology/bibo/>
+        PREFIX prism: <http://prismstandard.org/namespaces/basic/2.1/>
+        PREFIX schema: <http://schema.org/>
+        PREFIX md: <http://www.w3.org/ns/md#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         
-        SELECT ?title ?description ?image ?license WHERE {{
+        SELECT ?title ?description ?image ?license ?date ?creator ?publisher ?parent WHERE {{
             OPTIONAL {{
-                <{URL}>    og:title|dct:title|skos:prefLabel|skos:altLabel|rdfs:label  ?title .
+                <{URL}>    og:title|dct:title|skos:prefLabel|skos:altLabel|rdfs:label|md:item/rdf:first/schema:headline  ?title .
             }} 
             OPTIONAL {{
-                <{URL}>    og:description|dct:description ?description .
+                <{URL}>    og:description|dct:description|md:item/rdf:first/schema:description ?description .
             }} 
             OPTIONAL {{
-                <{URL}>    og:image|foaf:depiction ?image .
+                <{URL}>    og:image|foaf:depiction|md:item/rdf:first/schema:image ?image .
             }}
             OPTIONAL {{
                 <{URL}>    cc:license ?license .
             }}
+            OPTIONAL {{
+                <{URL}>    dct:date ?date .
+            }}
+            OPTIONAL {{
+                <{URL}>    dct:creator/foaf:name|md:item/rdf:first/schema:author/schema:name ?creator .
+            }}
+            OPTIONAL {{
+                <{URL}>    dct:publisher ?publisher .
+            }}
+            OPTIONAL {{
+                <{URL}>    dct:isPartOf/dct:title ?parent .
+            }}
+            
         }}
     
     """.format(URL = url)
@@ -65,9 +82,30 @@ def retrieve():
     print query 
     
     if url:
+        r = requests.get(url, headers=headers)
+        
+        ## Determine the final URL, if we went through a couple of redirects
+        final_url = r.url
+        host = re.match(r'(http://.*?)/.*',final_url).group(1)
+        
+        if image and not image.startswith('http'):
+            image = host + image
+            
+            image_r = requests.get(image)
+            if image_r.status_code >= 400:
+                image = None
+        
+        ## Determine the Content Type of the response
+        content_type = r.headers['content-type']
+        if ';' in content_type:
+            (content_type,_) = content_type.split(';',1)
+        
+        
+        ## Run rdflib parser on URL
+        
         graph = Graph()
 
-        graph.parse(data=r.content,publicID=url, format=content_type,media_type=r.headers['content-type'])
+        graph.parse(data=r.content,publicID=url, format=content_type)
         
         graph.bind('prov',PROV)
         
@@ -81,26 +119,43 @@ def retrieve():
         
         result = graph.query(query)
         
-        for (rdf_title, rdf_description, rdf_image, rdf_license) in result:
+        turtle = graph.serialize(format='turtle')
+        
+        
+        for (rdf_title, rdf_description, rdf_image, rdf_license, rdf_date, rdf_creator, rdf_publisher, rdf_parent ) in result:
             if rdf_title:
                 title = unicode(rdf_title)
             if rdf_description:
                 description = unicode(rdf_description)
             if rdf_image :
+                print rdf_image
                 image = unicode(rdf_image)
             if rdf_license :
                 license = unicode(rdf_license)
-        
-        turtle = graph.serialize(format='turtle')
+            if rdf_date :
+                date = unicode(rdf_date)
+            if rdf_creator :
+                creator = unicode(rdf_creator)
+            if rdf_publisher :
+                publisher = unicode(rdf_publisher)
+            if rdf_parent :
+                parent = unicode(rdf_parent)
         
         response = {
             'rdf': turtle,
+            'url': url,
+            'host': host,
+            'final_url': final_url,
             'title': title,
             'description': description,
             'image': image,
-            'url': url,
-            'license': license
+            'license': license,
+            'date': date,
+            'creator': creator,
+            'publisher': publisher,
+            'parent': parent
         }
+        
         
         
         return jsonify(response)
