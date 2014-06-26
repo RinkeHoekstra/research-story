1708840359fa7973b2cd18ac1ee45f1521b950f8
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-06-26 15:15:36 +0200

    OWL inferencing and dealing with weird ELSEVIER redirects

--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, make_response, jsonify
-from rdflib import Graph, Namespace
+from rdflib import Graph, Namespace, OWL, URIRef
+from bs4 import BeautifulSoup
 import requests
 import re
 import urllib
@@ -21,17 +22,20 @@ def index():
 @app.route('/retrieve',methods=['GET'])
 def retrieve():
     url = request.args.get('url',None)
-    title = request.args.get('title',None)
-    description = request.args.get('description',None)
-    image = request.args.get('image',None)
+    # title = request.args.get('title',None)
+    # description = request.args.get('description',None)
+    # image = request.args.get('image',None)
+    title = None
+    description = None
+    image = set()
     license = None
     date = None
     creator = set()
     parent = None
     publisher = None
     
-    headers = {'Accept':'application/rdf+xml;q=0.5, application/xhtml+xml;q=0.3, text/xml;q=0.2,application/xml;q=0.2, text/html;q=0.3, text/plain;q=0.1, text/n3;q=0.5, text/rdf+n3;q=0.5, application/x-turtle;q=0.8, text/turtle;q=1'}
-    
+    rdf_headers = {'Accept':'application/rdf+xml;q=0.5, application/xhtml+xml;q=0.3, text/xml;q=0.2,application/xml;q=0.2, text/html;q=0.3, text/plain;q=0.1, text/n3;q=0.5, text/rdf+n3;q=0.5, application/x-turtle;q=0.8, text/turtle;q=1'}
+    html_headers = {'Accept': 'application/xhtml+xml, text/xml, text/html'}
     
     
     
@@ -83,35 +87,95 @@ def retrieve():
     print query 
     
     if url:
-        r = requests.get(url, headers=headers)
-        
-        ## Determine the final URL, if we went through a couple of redirects
-        final_url = r.url
-        host = re.match(r'(http://.*?)/.*',final_url).group(1)
-        
-        if image and not image.startswith('http'):
-            image = host + image
-            
-            image_r = requests.get(image)
-            if image_r.status_code >= 400:
-                image = None
-        
-        ## Determine the Content Type of the response
-        content_type = r.headers['content-type']
-        if ';' in content_type:
-            (content_type,_) = content_type.split(';',1)
-        
-        
-        ## Run rdflib parser on URL
-        
         graph = Graph()
-
-        graph.parse(data=r.content,publicID=url, format=content_type)
         
+        graph.bind('owl',OWL)
         graph.bind('prov',PROV)
         
+        final_rdf_url = None
+        final_html_url = None
+        rdf_host = None
+        html_host = None
+        
+        rdf_r = requests.get(url, headers=rdf_headers)
+        
+        if rdf_r.status_code < 400 :
+            ## Determine the final URL, if we went through a couple of redirects
+            final_rdf_url = rdf_r.url
+            
+            try: 
+                rdf_host = re.match(r'(http://.*?)/.*',final_rdf_url).group(1)
+            except :
+                print "No match for RDF host"
+                rdf_host = final_rdf_url
+            
+            print "RDF Host", rdf_host
+            
+            ## Determine the Content Type of the response
+            content_type = rdf_r.headers['content-type']
+            if ';' in content_type:
+                (content_type,_) = content_type.split(';',1)
+        
+            ## Run rdflib parser on URL
+            graph.parse(data=rdf_r.content,publicID=url, format=content_type)
+        
+        
+        html_r = requests.get(url, headers=html_headers)
+        
+        
+        if html_r.status_code < 400 :
+            ## Determine the final URL, if we went through a couple of redirects
+            final_html_url = html_r.url
+            
+            if 'linkinghub.elsevier.com' in final_html_url:
+                print "Found ELSEVIER Linkinghub Redirect"
+                soup = BeautifulSoup(html_r.content)
+                redirect_input = soup.find(id="redirectURL")
+                redirect_url = redirect_input['value']
+                
+                html_r = requests.get(redirect_url, headers=html_headers)
+                
+                final_html_url = html_r.url
+                
+                
+            
+            
+            html_host = re.match(r'(http://.*?)/.*',final_html_url).group(1)
+            
+            print "HTML Host", html_host
+            
+            ## Determine the Content Type of the response
+            content_type = html_r.headers['content-type']
+            if ';' in content_type:
+                (content_type,_) = content_type.split(';',1)
+        
+            ## Run rdflib parser on URL
+            graph.parse(data=html_r.content,publicID=final_html_url, format=content_type)   
+            
+    
+        if final_html_url and url != final_html_url:
+            print "Adding sameAs"
+            
+            graph.add((URIRef(url),OWL['sameAs'],URIRef(final_html_url)))
+        
+
+        
+        
+        
+        
+        graph = naive_owl_sameas(graph)
+        
+        
+
+        
+
+        print graph.serialize(format='turtle')
+        
+        
+
+        
         for s,p,o in graph.triples( (None, PROV['wasGeneratedBy'], None) ):
-            r = requests.get(url, headers=headers)
+            r = requests.get(url, headers=rdf_headers)
     
             content_type = r.headers['content-type']
             graph.parse(data=r.content,format=content_type)
@@ -129,8 +193,10 @@ def retrieve():
             if rdf_description:
                 description = unicode(rdf_description)
             if rdf_image :
-                print rdf_image
-                image = unicode(urllib.unquote(rdf_image))
+                print "Image: ", rdf_image
+                image.add(unicode(urllib.unquote(rdf_image)))
+                
+                
             if rdf_license :
                 license = unicode(urllib.unquote(rdf_license))
             if rdf_date :
@@ -145,11 +211,13 @@ def retrieve():
         response = {
             'rdf': turtle,
             'url': url,
-            'host': host,
-            'final_url': final_url,
+            'html_host': html_host,
+            'rdf_host': rdf_host,
+            'final_rdf_url': final_rdf_url,
+            'final_html_url': final_html_url,
             'title': title,
             'description': description,
-            'image': image,
+            'image': list(image),
             'license': license,
             'date': date,
             'creator': list(creator),
@@ -163,4 +231,37 @@ def retrieve():
     else :
         print "ERROR"
         return "ERROR"
+        
+        
+def naive_owl_sameas(graph):
     
+    for one,p,two in graph.triples((None,OWL['sameAs'],None)):
+        
+        ## Forward one step
+        for s,p,o in graph.triples((one, None, None)):
+            graph.add((two,p,o))
+        for s,p,o in graph.triples((two, None, None)):
+            graph.add((one,p,o))
+            
+        ## Backward one step
+        for s,p,o in graph.triples((None, None, one)):
+            graph.add((s,p,two))
+        for s,p,o in graph.triples((None, None, two)):
+            graph.add((s,p,one))
+            
+            
+    return graph
+
+
+
+
+
+
+
+
+
+
+
+
+
+
